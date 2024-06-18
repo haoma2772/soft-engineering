0dc0ea47ce3cb0045af04aa67abac743009b6cd3
Hao Ma <94169263+haoma2772@users.noreply.github.com>
2024-06-18 15:24:34 +0800

    Add files via upload

--- code/soft.sql
+++ code/soft.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 08/04/2024 17:40:08
+ Date: 18/06/2024 12:27:07
 */
 
 SET NAMES utf8mb4;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `app01_administrator`;
 CREATE TABLE `app01_administrator`  (
   `ad_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `ad_name` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ad_password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ad_password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `ad_gender` tinyint(1) NOT NULL,
   `ad_phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `ad_email` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -34,7 +34,10 @@ CREATE TABLE `app01_administrator`  (
 -- ----------------------------
 -- Records of app01_administrator
 -- ----------------------------
-INSERT INTO `app01_administrator` VALUES ('111', '1', '111', 1, '1', '1');
+INSERT INTO `app01_administrator` VALUES ('10010', '王二', '$2b$12$mewuTkI1DOBXgnfcw2ss0eZQYGlScrExX5KG0UuPERkxlkpDDBLUi', 1, '13355099080', 'gengzun@126.com');
+INSERT INTO `app01_administrator` VALUES ('111', '1', '$2b$12$RqVUyVB9p/tSxuEjkZ1XPOGLzEbSmgrlUlTNU6Bn7AP9mbrfO9gT6', 1, '1', '1');
+INSERT INTO `app01_administrator` VALUES ('123', '1', '$2b$12$4/y2cFZmKdEtKZZ5RygmGOVjD4d9ahWYseGY3BJJsgZa42E3chIly', 1, '1', 'gengzun@126.com');
+INSERT INTO `app01_administrator` VALUES ('202100130110', 'sduqx', '$2b$12$vTeBJgLCH4mQlSDcQ7fMsuFAbfmWQwmAhNY/LNMk7ZfvNPoxTtSzu', 1, '13355099080', '2775469595@qq.com');
 
 -- ----------------------------
 -- Table structure for app01_complaint
@@ -51,6 +54,7 @@ CREATE TABLE `app01_complaint`  (
 -- ----------------------------
 -- Records of app01_complaint
 -- ----------------------------
+INSERT INTO `app01_complaint` VALUES (1, '2024-06-17 10:14:14.000000', '1234', '321');
 
 -- ----------------------------
 -- Table structure for app01_default
@@ -68,6 +72,32 @@ CREATE TABLE `app01_default`  (
 -- ----------------------------
 -- Records of app01_default
 -- ----------------------------
+INSERT INTO `app01_default` VALUES (1, '早退', '24201', '111', '2024-06-17 11:31:12.000000');
+INSERT INTO `app01_default` VALUES (2, '泄露公司机密', '24201', '111', '2024-06-17 16:43:10.000000');
+
+-- ----------------------------
+-- Table structure for app01_payroll
+-- ----------------------------
+DROP TABLE IF EXISTS `app01_payroll`;
+CREATE TABLE `app01_payroll`  (
+  `payRoll_id` int NOT NULL,
+  `month` int NOT NULL,
+  `basicSalary` double NOT NULL,
+  `bonus` double NOT NULL,
+  `deductions` double NOT NULL,
+  `total` double NOT NULL,
+  `user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`payRoll_id`) USING BTREE,
+  INDEX `app01_payroll_user_id_5ba4f259_fk_app01_user_user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `app01_payroll_user_id_5ba4f259_fk_app01_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `app01_user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of app01_payroll
+-- ----------------------------
+INSERT INTO `app01_payroll` VALUES (24601, 6, 10000, 3000, 400, 12600, '24201');
+INSERT INTO `app01_payroll` VALUES (24603, 8, 8000, 2200, 200, 10000, '20242');
+INSERT INTO `app01_payroll` VALUES (24604, 6, 8000, 2200, 200, 10000, '20242');
 
 -- ----------------------------
 -- Table structure for app01_seat
@@ -86,7 +116,6 @@ CREATE TABLE `app01_seat`  (
 -- ----------------------------
 -- Records of app01_seat
 -- ----------------------------
-INSERT INTO `app01_seat` VALUES (104, 1, '东', '104', '有电源', '空闲中');
 
 -- ----------------------------
 -- Table structure for app01_usedseat
@@ -125,7 +154,7 @@ CREATE TABLE `app01_usedworkshop`  (
 DROP TABLE IF EXISTS `app01_user`;
 CREATE TABLE `app01_user`  (
   `user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `user_gender` tinyint(1) NOT NULL,
   `user_name` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `user_academy` int NOT NULL,
@@ -144,9 +173,11 @@ CREATE TABLE `app01_user`  (
 -- ----------------------------
 -- Records of app01_user
 -- ----------------------------
-INSERT INTO `app01_user` VALUES ('111', '111', 1, 'a', 6, 23, 1, '汉族', 0, '1', '1', '未婚', '工程师', '在岗');
-INSERT INTO `app01_user` VALUES ('123', '111', 0, 'g', 6, 22, 1, '汉族', 0, '13153629203', 'gengzunz@gmail.com', '未婚', '销售', '在岗');
-INSERT INTO `app01_user` VALUES ('124', '123456', 0, 'q', 6, 20, 2, '汉族', 0, 'XXXXXXXXXXX', 'XXX@XXX.com', '已婚', '销售', '在岗');
+INSERT INTO `app01_user` VALUES ('20242', '123456', 1, '李四', 6, 25, 3, '汉族', 0, 'XXXXXXXXXXX', 'XXX@XXX.com', '未婚', '销售', '在岗');
+INSERT INTO `app01_user` VALUES ('20243', '123456', 1, '王五', 5, 30, 10, '汉族', 0, 'XXXXXXXXXXX', 'XXX@XXX.com', '已婚', '销售', '在岗');
+INSERT INTO `app01_user` VALUES ('20244', '123456', 0, '孙六', 3, 30, 10, '汉族', 0, 'XXXXXXXXXXX', 'XXX@XXX.com', '已婚', '销售', '在岗');
+INSERT INTO `app01_user` VALUES ('20245', '123456', 1, '周七', 3, 30, 10, '汉族', 0, 'XXXXXXXXXXX', 'XXX@XXX.com', '未婚', '客服', '辞退');
+INSERT INTO `app01_user` VALUES ('24201', '123456', 1, '张四', 6, 25, 4, '汉族', 2, '13153629203', 'gengzun@126.com', '未婚', '工程师', '在岗');
 
 -- ----------------------------
 -- Table structure for app01_workshop
@@ -164,7 +195,6 @@ CREATE TABLE `app01_workshop`  (
 -- ----------------------------
 -- Records of app01_workshop
 -- ----------------------------
-INSERT INTO `app01_workshop` VALUES (101, 12, '12', '12', '空闲中');
 
 -- ----------------------------
 -- Table structure for auth_group
@@ -212,7 +242,7 @@ CREATE TABLE `auth_permission`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `auth_permission_content_type_id_codename_01ab375a_uniq`(`content_type_id` ASC, `codename` ASC) USING BTREE,
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 56 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 60 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of auth_permission
@@ -273,6 +303,10 @@ INSERT INTO `auth_permission` VALUES (53, 'Can add workshop', 14, 'add_workshop'
 INSERT INTO `auth_permission` VALUES (54, 'Can change workshop', 14, 'change_workshop');
 INSERT INTO `auth_permission` VALUES (55, 'Can delete workshop', 14, 'delete_workshop');
 INSERT INTO `auth_permission` VALUES (56, 'Can view workshop', 14, 'view_workshop');
+INSERT INTO `auth_permission` VALUES (57, 'Can add payroll', 15, 'add_payroll');
+INSERT INTO `auth_permission` VALUES (58, 'Can change payroll', 15, 'change_payroll');
+INSERT INTO `auth_permission` VALUES (59, 'Can delete payroll', 15, 'delete_payroll');
+INSERT INTO `auth_permission` VALUES (60, 'Can view payroll', 15, 'view_payroll');
 
 -- ----------------------------
 -- Table structure for auth_user
@@ -371,7 +405,7 @@ CREATE TABLE `django_content_type`  (
   `model` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `django_content_type_app_label_model_76bd3d3b_uniq`(`app_label` ASC, `model` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of django_content_type
@@ -380,6 +414,7 @@ INSERT INTO `django_content_type` VALUES (1, 'admin', 'logentry');
 INSERT INTO `django_content_type` VALUES (7, 'app01', 'administrator');
 INSERT INTO `django_content_type` VALUES (8, 'app01', 'complaint');
 INSERT INTO `django_content_type` VALUES (9, 'app01', 'default');
+INSERT INTO `django_content_type` VALUES (15, 'app01', 'payroll');
 INSERT INTO `django_content_type` VALUES (10, 'app01', 'seat');
 INSERT INTO `django_content_type` VALUES (11, 'app01', 'usedseat');
 INSERT INTO `django_content_type` VALUES (12, 'app01', 'usedworkshop');
@@ -401,30 +436,30 @@ CREATE TABLE `django_migrations`  (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of django_migrations
 -- ----------------------------
-INSERT INTO `django_migrations` VALUES (1, 'contenttypes', '0001_initial', '2024-03-21 02:30:05.570302');
-INSERT INTO `django_migrations` VALUES (2, 'auth', '0001_initial', '2024-03-21 02:30:06.065901');
-INSERT INTO `django_migrations` VALUES (3, 'admin', '0001_initial', '2024-03-21 02:30:06.195969');
-INSERT INTO `django_migrations` VALUES (4, 'admin', '0002_logentry_remove_auto_add', '2024-03-21 02:30:06.205103');
-INSERT INTO `django_migrations` VALUES (5, 'admin', '0003_logentry_add_action_flag_choices', '2024-03-21 02:30:06.216041');
-INSERT INTO `django_migrations` VALUES (6, 'app01', '0001_initial', '2024-03-21 02:30:06.335373');
-INSERT INTO `django_migrations` VALUES (7, 'contenttypes', '0002_remove_content_type_name', '2024-03-21 02:30:06.427731');
-INSERT INTO `django_migrations` VALUES (8, 'auth', '0002_alter_permission_name_max_length', '2024-03-21 02:30:06.491109');
-INSERT INTO `django_migrations` VALUES (9, 'auth', '0003_alter_user_email_max_length', '2024-03-21 02:30:06.519080');
-INSERT INTO `django_migrations` VALUES (10, 'auth', '0004_alter_user_username_opts', '2024-03-21 02:30:06.528060');
-INSERT INTO `django_migrations` VALUES (11, 'auth', '0005_alter_user_last_login_null', '2024-03-21 02:30:06.593274');
-INSERT INTO `django_migrations` VALUES (12, 'auth', '0006_require_contenttypes_0002', '2024-03-21 02:30:06.597892');
-INSERT INTO `django_migrations` VALUES (13, 'auth', '0007_alter_validators_add_error_messages', '2024-03-21 02:30:06.606870');
-INSERT INTO `django_migrations` VALUES (14, 'auth', '0008_alter_user_username_max_length', '2024-03-21 02:30:06.670977');
-INSERT INTO `django_migrations` VALUES (15, 'auth', '0009_alter_user_last_name_max_length', '2024-03-21 02:30:06.739685');
-INSERT INTO `django_migrations` VALUES (16, 'auth', '0010_alter_group_name_max_length', '2024-03-21 02:30:06.765875');
-INSERT INTO `django_migrations` VALUES (17, 'auth', '0011_update_proxy_permissions', '2024-03-21 02:30:06.780317');
-INSERT INTO `django_migrations` VALUES (18, 'auth', '0012_alter_user_first_name_max_length', '2024-03-21 02:30:06.845252');
-INSERT INTO `django_migrations` VALUES (19, 'sessions', '0001_initial', '2024-03-21 02:30:06.880176');
+INSERT INTO `django_migrations` VALUES (1, 'contenttypes', '0001_initial', '2024-06-03 07:30:15.574396');
+INSERT INTO `django_migrations` VALUES (2, 'auth', '0001_initial', '2024-06-03 07:30:15.953851');
+INSERT INTO `django_migrations` VALUES (3, 'admin', '0001_initial', '2024-06-03 07:30:16.031934');
+INSERT INTO `django_migrations` VALUES (4, 'admin', '0002_logentry_remove_auto_add', '2024-06-03 07:30:16.038915');
+INSERT INTO `django_migrations` VALUES (5, 'admin', '0003_logentry_add_action_flag_choices', '2024-06-03 07:30:16.045717');
+INSERT INTO `django_migrations` VALUES (6, 'app01', '0001_initial', '2024-06-03 07:30:16.175277');
+INSERT INTO `django_migrations` VALUES (7, 'contenttypes', '0002_remove_content_type_name', '2024-06-03 07:30:16.238915');
+INSERT INTO `django_migrations` VALUES (8, 'auth', '0002_alter_permission_name_max_length', '2024-06-03 07:30:16.278634');
+INSERT INTO `django_migrations` VALUES (9, 'auth', '0003_alter_user_email_max_length', '2024-06-03 07:30:16.299380');
+INSERT INTO `django_migrations` VALUES (10, 'auth', '0004_alter_user_username_opts', '2024-06-03 07:30:16.307360');
+INSERT INTO `django_migrations` VALUES (11, 'auth', '0005_alter_user_last_login_null', '2024-06-03 07:30:16.356596');
+INSERT INTO `django_migrations` VALUES (12, 'auth', '0006_require_contenttypes_0002', '2024-06-03 07:30:16.359627');
+INSERT INTO `django_migrations` VALUES (13, 'auth', '0007_alter_validators_add_error_messages', '2024-06-03 07:30:16.367606');
+INSERT INTO `django_migrations` VALUES (14, 'auth', '0008_alter_user_username_max_length', '2024-06-03 07:30:16.416397');
+INSERT INTO `django_migrations` VALUES (15, 'auth', '0009_alter_user_last_name_max_length', '2024-06-03 07:30:16.465583');
+INSERT INTO `django_migrations` VALUES (16, 'auth', '0010_alter_group_name_max_length', '2024-06-03 07:30:16.482483');
+INSERT INTO `django_migrations` VALUES (17, 'auth', '0011_update_proxy_permissions', '2024-06-03 07:30:16.493473');
+INSERT INTO `django_migrations` VALUES (18, 'auth', '0012_alter_user_first_name_max_length', '2024-06-03 07:30:16.541822');
+INSERT INTO `django_migrations` VALUES (19, 'sessions', '0001_initial', '2024-06-03 07:30:16.569253');
 
 -- ----------------------------
 -- Table structure for django_session
@@ -441,6 +476,6 @@ CREATE TABLE `django_session`  (
 -- ----------------------------
 -- Records of django_session
 -- ----------------------------
-INSERT INTO `django_session` VALUES ('xyb48onwph9t0dyli7h9txp4c9vha65o', 'eyJpc19sb2dpbiI6dHJ1ZSwibmFtZSI6IjEyMyIsInBhc3N3b3JkIjoiMTExIn0:1rtkoQ:xY8vTJxIgDRjvUpQYuViBTZ58xIbzp7e1W29Iczdcgg', '2024-04-22 08:57:10.207965');
+INSERT INTO `django_session` VALUES ('zrcrn85tqypkqvxyupjs22my6axek0d4', '.eJxNjF0LgjAYRv_L8DJiWxblVYgiUdCnUN2IzTlenJtsLqXov7fuujzPOTxvBFVBUIQIxgSjCeJtCdKz4Eq8nFoTupgy3Xrz5AZqYGUPWhVMV9xX4WxFw6WXYAupBSgU9cZxz9XfZ1daO2jzmwL6CAgNWj64S7MhyT6-ClWzgVqL-f14y-SZmXS8zrcZzt0hPTWjbLokiXc5oM8X-Og2HA:1sJ82S:JGIZ4QQ0Gei22co535RRAc4ZuOeVw6g9C_TfNKeXcQg', '2024-07-01 08:48:32.612868');
 
 SET FOREIGN_KEY_CHECKS = 1;
